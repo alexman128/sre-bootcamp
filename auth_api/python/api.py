97c15e4330d12bfae0f9a249cc5416787e81bad0
Manuel Alejand Mijangos Gonzal <alejandro@MacBook-Air-de-Manuel.local>
2021-09-04 12:11:37 -0500

    version 0.1

--- auth_api/python/api.py
+++ auth_api/python/api.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from methods import Token, Restricted
+import hashlib
+#from flask_mysqldb import MySQL
+import mysql.connector
+
 
 app = Flask(__name__)
+
+#mysql = MySQL(app)
 login = Token()
 protected = Restricted()
 
+app.config['secret'] = "my2w7wjd7yXF64FIADfJxNs1oupTGAuW"
 
 # Just a health check
 @app.route("/")
@@ -21,13 +28,53 @@ def url_health():
 
 
 # e.g. http://127.0.0.1:8000/login
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST', 'GET'])
 def url_login():
-    username = request.form['username']
-    password = request.form['password']
-    res = {
-        "data": login.generate_token(username, password)
-    }
+    if request.method == 'GET':
+
+        #username = request.form['username']
+        #password = request.form['password']
+        username = request.args.get('username')
+        password = request.args.get('password')        
+        
+        
+        #print(f"username {username}, password {password}")
+    
+        Conection_db = mysql.connector.connect( 
+            host='bootcamp-tht.sre.wize.mx', 
+            user= 'secret', 
+            passwd='noPow3r', 
+            port=3306, 
+            db='bootcamp_tht' )
+        cur = Conection_db.cursor()
+
+        cur.execute( "SELECT username, password, salt FROM users  WHERE username = %s", (username, ))
+        data = cur.fetchall()
+        if data:
+            row = data[0]
+            encrypted_password = row[1]
+            salt = str(row[2])
+            salted = str(password) + salt
+        
+
+            #print(f"encrypted password {encrypted_password}")
+            salted = salted.encode('utf-8')
+            h = hashlib.sha512(salted)
+            encrypted =  h.hexdigest()
+            print(f"encrypted {encrypted}")
+            print(f"encrypted stored in the database: {encrypted_password}")
+
+            if (encrypted == encrypted_password):
+                res = {
+                       "data": login.generate_token(username, password)
+                }
+            else:
+                res = None
+        else: 
+            print("No data")
+            res = {'Error': 'wrong credentials'}
+
+    
     return jsonify(res)
 
 
--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,11 +1,42 @@
+import jwt
+import jsonify
+import sys
+
+secret = "my2w7wjd7yXF64FIADfJxNs1oupTGAuW"
+
 # These functions need to be implemented
 class Token:
 
     def generate_token(self, username, password):
-        return 'test'
+        #puse ese payload pero no estoy seguro si el correcto es el que ustedes usaron en su ejemplo:
+        #aunque en las instrucciones dice que el payload debe de ser el rol y en este caso seria el username
+        '''
+                {
+        "sub": "1234567890",
+        "name": "John Doe",
+        "iat": 1516239022
+        }
+        '''
+
+        encoded_jwt = jwt.encode({"username": username}, secret, algorithm="HS256")
+
+        print(encoded_jwt)
+        
+        return encoded_jwt
 
 
 class Restricted:
 
     def access_data(self, authorization):
-        return 'test'
+        if not authorization:
+            return 'token is missing'
+        try:
+            print(f"secret: {secret}")
+            print(f"authorization: {authorization}")
+            data = jwt.decode(authorization, secret, algorithms=["HS256"])
+        except:
+            e = sys.exc_info()[0]
+            print(f"Exception: {e}")
+            return 'wrong token'
+
+        return 'You are under protected data'
